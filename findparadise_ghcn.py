41c11cb7b79e8aa49c50d99ba208ab83924c70b7
PascalSteger <psteger@phys.ethz.ch>
2015-04-18 18:50:00 +0200

    all stations with TSUN measurements; plotting earth projection with ipython2

--- findparadise_ghcn.py
+++ findparadise_ghcn.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env ipython2
 
 ##
 # @file
@@ -9,8 +9,38 @@
 # (c) GPL v3 2015 ETHZ Pascal S.P. Steger pascal@steger.aero
 
 import pdb
+import matplotlib.pyplot as plt
 import numpy as np
 import os
+
+import mpl_toolkits
+from mpl_toolkits.basemap import Basemap, cm
+import matplotlib.pyplot as plt
+
+# set up orthographic map projection with
+# perspective of satellite looking down at 50N, 100W.
+# use low resolution coastlines.
+# don't plot features that are smaller than 1000 square km.
+# projection = 'ortho' gives round earth
+# woll, hammer, robin
+map = Basemap(projection='robin', lat_0 = 0, lon_0 = 0, resolution = 'c')
+# map = Basemap(projection='cass', lat_0 = -42, lon_0 = 174,
+#               resolution = 'i', width=1591185,height=3115557)
+
+def plot_earth():
+    # draw coastlines, country boundaries, fill continents.
+    map.drawcoastlines(linewidth=0.5, color='gray')
+    map.drawcountries(linewidth=0.5, color='#aaaaaa')
+    #map.fillcontinents() #color = 'coral')
+    # or instead, use imagery from NASA
+    #map.bluemarble()
+
+    # draw the edge of the map projection region (the projection limb)
+    map.drawmapboundary()
+    # draw lat/lon grid lines every 30 degrees.
+    #map.drawmeridians(np.arange(0, 360, 15))
+    #map.drawparallels(np.arange(-90, 90, 15))
+    return
 
 def pretty(scal, dig=2):
     return "%.*f" % (dig-1, scal)
@@ -87,12 +117,17 @@ def step2():
     print(sum(sel), 'stations fulfill our criteria')
 
 
+
+
 if __name__ == "__main__":
+    plot_earth()
+    plt.show()
+    pdb.set_trace()
     with open('./ghcn/ghcnd-stations.txt', 'r') as stationfile:
         for line in stationfile.readlines():
             #if line[:2] != 'SZ':
             #    continue
-            print(line)
+            #print(line)
             station = line[0:11]
             deg_north = float(line[11:20])
             deg_east  = float(line[20:30])

089a9f24127bdd7f4b6a7eeec791c69b178f525f
PascalSteger <psteger@phys.ethz.ch>
2015-04-18 17:57:22 +0200

    all stations automated

--- findparadise_ghcn.py
+++ findparadise_ghcn.py
@@ -19,13 +19,14 @@ def split(line):
     offset = 21
     values = []
     for k in range(31):
-        values.append(int(line[offset:offset+5]))
+        li = int(line[offset:offset+5])
+        if li >= 0.0:
+            values.append(li)
         offset += 8
     values = np.array(values)
     return values, values.sum()
 
-def find_TSUN_monthlymean():
-    fname = './ghcn/ghcnd_all/'+'FMW00040505.dly'
+def find_TSUN_monthlymean(fname):
     # does file exist?
     if not os.path.isfile(fname):
         print("File "+fname+" does not exist!")
@@ -33,24 +34,25 @@ def find_TSUN_monthlymean():
     # now we know that file exists
 
     # we open the file and read from it:
+    median_tsun = np.nan
     monthly_median_tsun = [] # [h] <= measured in units of hours
     total_sunshine = 0 # [h]
     with open(fname , "r") as fi:
         for line in fi.readlines():
             #print(line[17:21])
             #continue
-
             if line[17:21] == 'TSUN':
                 values, total = split(line) # [min]
                 total_sunshine += total/60 # [h]
-
                 median_tsun = np.median(values)/60
                 #print(line[11:17]+" had a median sunshine duration of "+str(median_tsun)+" hours.")
                 monthly_median_tsun.append(median_tsun)
     #print(monthly_median_tsun)
-    median_tsun = np.median(monthly_median_tsun[-12:])
-    print(fname+" has a "+str(pretty(median_tsun))+" hours of sunlight on a normal day in a normal month and a total of "+str(int(total_sunshine))+" hours in a year.")
-    print(total_sunshine/median_tsun/365.25)
+    #if len(monthly_median_tsun) > 0:
+    #    median_tsun = np.median(monthly_median_tsun[-12:])
+    #    print(fname+" "+str(pretty(median_tsun))+" "+str(int(total_sunshine)))
+    #    #print(total_sunshine/median_tsun/365.25)
+    return median_tsun, total_sunshine
 
 def step2():
     dat = 'minmaxmed.dat'
@@ -85,6 +87,16 @@ def step2():
     print(sum(sel), 'stations fulfill our criteria')
 
 
-if __name__=="__main__":
-    find_TSUN_monthlymean()
-    #step2() # for finding the stations with agreeable temperature range
+if __name__ == "__main__":
+    with open('./ghcn/ghcnd-stations.txt', 'r') as stationfile:
+        for line in stationfile.readlines():
+            #if line[:2] != 'SZ':
+            #    continue
+            print(line)
+            station = line[0:11]
+            deg_north = float(line[11:20])
+            deg_east  = float(line[20:30])
+            fname = './ghcn/ghcnd_all/' + station + '.dly'
+            med_tsun, tot_sunshine = find_TSUN_monthlymean(fname)
+            if not np.isnan(med_tsun):
+                print(station+" "+str(pretty(med_tsun))+" "+str(tot_sunshine)+" "+line[40:])
